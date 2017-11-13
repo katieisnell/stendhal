a1c7e70cd765696f260e13eebed22a5bcf173f83
Tudor Ruxandu <tudor.ruxandu@student.manchester.ac.uk>
2017-11-13 20:03:04 +0000

    Changed BankScroll tests to fit design changes
    Changed the methods in BankScrollTest in order to match the design and
    the implementation of BankScroll. Assumed that we use the entity manager
    to get the scroll, and that we have a method for changing the info
    string of the scroll.

--- tests/games/stendhal/server/entity/item/scroll/BankScrollTest.java
+++ tests/games/stendhal/server/entity/item/scroll/BankScrollTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.PassiveEntity;
+import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.item.Money;
 import games.stendhal.server.entity.player.Player;
 import utilities.PlayerTestHelper;
@@ -13,7 +15,7 @@ import utilities.PlayerTestHelper;
 
 public class BankScrollTest {
 	
-	static Player player;
+	private static Player player;
 	
 	@BeforeClass
 	public static void setUpBeforeClass()
@@ -25,21 +27,24 @@ public class BankScrollTest {
 	public void shouldReturnChestContents()
 	{
 		PassiveEntity money = new Money(200);
+		PassiveEntity sword = new Item("sword", "", "", null);
 		String bank = "bank";
 		player.getSlot(bank).add(money);
-
-		BankScroll scroll = new BankScroll(player, bank);
-		
+		player.getSlot(bank).add(sword);
+		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
+		scroll.setInfo(player,bank);
+		player.getSlot("bag").add(scroll);
 		scroll.useScroll(player);
-		assertEquals(bank + " chest has: 200 money", PlayerTestHelper.getPrivateReply(player));
-		
+		assertEquals("Semos bank chest contains 200 money, 1 sword", PlayerTestHelper.getPrivateReply(player));
 	}
 	
 	@Test
 	public void shouldRemainInBag()
 	{
 		String bank = "bank";
-		BankScroll scroll = new BankScroll(player, bank);
+		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
+		scroll.setInfo(player,bank);
+		player.equip("bag", scroll);
 		scroll.useScroll(player);
 		assertTrue(scroll.equals(player.getSlot("bag").getFirst()));
 	}
