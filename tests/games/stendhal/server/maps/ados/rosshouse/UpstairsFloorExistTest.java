cafa3c685765e6444e31587ff2e11da3384cdb04
Minh Khoi Nguyen <minh.nguyen-4@student.manchester.ac.uk>
2017-11-16 18:39:27 +0000

    Fix indentation error and update UpstairsFloorExistTest.java

--- tests/games/stendhal/server/maps/ados/rosshouse/UpstairsFloorExistTest.java
+++ tests/games/stendhal/server/maps/ados/rosshouse/UpstairsFloorExistTest.java
@@ -18,30 +18,29 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class UpstairsFloorExistTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		QuestHelper.setUpBeforeClass();
-		// Load the zones in ados before the test case.
-		ZonesXMLLoader zonesInAdos = new ZonesXMLLoader(new URI("/data/conf/zones/ados.xml"));
-		zonesInAdos.load();
-	}
-	
-	@Test
-	public void shouldHaveUpstairsFloor() {	
-		// Create the RPworld that contain all the zones.
-		StendhalRPWorld gameWorld = SingletonRepository.getRPWorld();
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        QuestHelper.setUpBeforeClass();
+        // Load the zones in ados before the test case.
+        ZonesXMLLoader zonesInAdos = new ZonesXMLLoader(new URI("/data/conf/zones/ados.xml"));
+        zonesInAdos.load();
+    }
 
-		// Get the upstairs interior in Ross house from all the zones.
-		IRPZone ados_house_upstair = gameWorld.getRPZone("int_ados_ross_house_1"); 
-		
-		// Check if Ross's upstair floor zone is not null.
-		assertNotNull(ados_house_upstair);
-		
-		// Load all the interior zones in ados that is accessible.
-		Collection<StendhalRPZone> ados_interiors_all =  gameWorld.getAllZonesFromRegion("ados", false, null, true);
+    @Test
+    public void shouldHaveUpstairsFloor() {
+    	// Create the RPworld that contain all the zones.
+    	StendhalRPWorld gameWorld = SingletonRepository.getRPWorld();
 
-		// Check if the interior zone in ados contains the upstairs floor in Ross house.
-		assertThat(ados_interiors_all.contains(ados_house_upstair), is(true));
-	}
+    	// Get the upstairs interior in Ross house from all the zones.
+    	IRPZone ados_house_upstair = gameWorld.getRPZone("int_ados_ross_house_1");
+
+    	// Check if Ross's upstair floor zone is not null.
+    	assertNotNull(ados_house_upstair);
+
+    	// Load all the interior zones in ados that is accessible.
+    	Collection<StendhalRPZone> ados_interiors_all =  gameWorld.getAllZonesFromRegion("ados", false, null, true);
+
+    	// Check if the interior zone in ados contains the upstairs floor in Ross house.
+    	assertThat(ados_interiors_all.contains(ados_house_upstair), is(true));
+    }
 }
-
