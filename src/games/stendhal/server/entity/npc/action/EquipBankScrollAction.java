2031147e869a52ba78a0a23f3a61eee97a7b0cc6
Diana <diana.pislaru@student.manchester.ac.uk>
2017-11-17 12:00:49 +0000

    Added comments to EquipBankScrollAction and EquipBankScrollActionTest

--- src/games/stendhal/server/entity/npc/action/EquipBankScrollAction.java
+++ src/games/stendhal/server/entity/npc/action/EquipBankScrollAction.java
@@ -8,9 +8,11 @@ import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.player.Player;
 
-/**
- * Equips the player with a bank scroll.
- */
+/* Equips the player with a BankScroll
+ * 
+ * author: Diana Pislaru
+ * 
+ * */
 public class EquipBankScrollAction implements ChatAction {
 	
 	private final String bankName;
@@ -26,12 +28,18 @@ public class EquipBankScrollAction implements ChatAction {
 		this.bankName = bankName;
 	}
 	
+	/* Equips the player with a new BankScroll */
 	@Override
 	public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
 
+		// Create a BankScroll
 		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem(itemName);
+		
+		// Link the BankScroll to the correct bank
 		scroll.setInfo(player, bankName);
 		scroll.setQuantity(1);
+		
+		// Equip the player with the BankScroll and notify the game
 		player.equipOrPutOnGround(scroll);
 		TutorialNotifier.equippedByNPC(player, scroll);
 		player.notifyWorldAboutChanges();
--- tests/games/stendhal/server/entity/npc/action/EquipBankScrollActionTest.java
+++ tests/games/stendhal/server/entity/npc/action/EquipBankScrollActionTest.java
@@ -13,22 +13,37 @@ import marauroa.common.Log4J;
 import marauroa.server.game.db.DatabaseFactory;
 import utilities.PlayerTestHelper;
 
+/* Tests for EquipBankScrollActionTest
+ * 
+ * author: Diana Pislaru
+ * 
+ * */
 public class EquipBankScrollActionTest {
+	
 	@BeforeClass
 	public static void beforeClass() {
 		Log4J.init();
 		MockStendlRPWorld.get();
 		new DatabaseFactory().initializeDatabase();
 	}
-
+	
+	/* Test the fire method */
 	@Test
 	public void testFire() {
+		// Create a player
 		Player p = PlayerTestHelper.createPlayer("bob");
+		
+		// Test that the player does not have any bank scrolls
 		assertThat(Boolean.valueOf(p.isEquipped("bank scroll")), is(Boolean.FALSE));
+		
+		// Create the EquipBankScrollAction with a BankScroll from Ados Bank
 		EquipBankScrollAction action = new EquipBankScrollAction("bank_ados");
+		
+		// Call fire method for the player
 		action.fire(p, null, null);
+		
+		// Test that the player has the correct BankScroll
 		assertThat(Boolean.valueOf(p.isEquipped("bank scroll")), is(Boolean.TRUE));
 		assertEquals(p.getFirstEquipped("bank scroll").getInfoString(), p.getName() + " " + "bank_ados");
-		
 	}
 }
