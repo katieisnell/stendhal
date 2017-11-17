70a86e0381d638967259484f2f7a052d72b71d8d
Tudor Ruxandu <tudor.ruxandu@student.manchester.ac.uk>
2017-11-17 10:12:51 +0000

    Added comments to the BankScroll and BankScrollTest classes
    Added comments to the specified classes in order to ensure continuity
    with other classes in stendhal. Also added a few other statements in the
    BankScrollTest class to check if a bank scroll also informs the player
    correctly when the bank chest is empty

--- src/games/stendhal/server/entity/item/scroll/BankScroll.java
+++ src/games/stendhal/server/entity/item/scroll/BankScroll.java
@@ -7,26 +7,58 @@ import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPObject;
 
+/**
+ * Represents a scroll that returns the contents of an associated bank chest
+ * as a private message when used
+ * @author mbaxatr2
+ */
 public class BankScroll extends PersistentScroll{
 	
+	// Associated values for a bank scroll
 	private String playerName = null;
 	private String bankName;
 	private String bankNameMapping;
 
+	/**
+	 * Creates a new scroll.
+	 *
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
 	public BankScroll(final String name, final String clazz, final String subclass,
 			final Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 	}
 
+	/**
+	 * Copy constructor.
+	 *
+	 * @param item
+	 *            item to copy
+	 */
 	public BankScroll(final BankScroll item) {
 		super(item);
 	}
 	
+	/**
+	 * Use a scroll.
+	 *
+	 * @param player
+	 *            The player using the scroll.
+	 *
+	 * @return <code>true</code> if successful, <code>false</code>
+	 *         otherwise.
+	 */
 	@Override
 	public boolean useScroll(final Player player) {
+		//Split the info string to get the player name and the bank name
 		String[] info = getInfoString().split(" ");
 		playerName = info[0];
 		bankName = info[1];
+		
+		// Map the bank name to a player friendly form
 		switch (bankName)
 		{
 		case "bank":
@@ -42,30 +74,45 @@ public class BankScroll extends PersistentScroll{
 		default:
 			bankNameMapping = "Semos bank chest"; break;
 		}
+		
+		// If the player using the scroll is the owner
 		if (player.getName().equals(playerName)){
+			// Create an iterator for the bank chest and a string that will store the
+			// bank statement
 			Iterator<RPObject> iterator = player.getSlot(bankName).iterator();
 			String contents;
+			
+			// If there is an item in the chest
 			if (iterator.hasNext())
 			{
+				// Add it to the bank statement with its quantity
 				Item item = (Item)iterator.next();
 				contents = item.getQuantity() + " " + item.getName();
 				
+				// While there are more items in the chest
 				while (iterator.hasNext())
 				{
+					// Add them to the bank statement
 					item = (Item)iterator.next();
 					contents += ", " + item.getQuantity() + " " + item.getName();
 				}
 			}
+			// otherwise make the bank statement say the chest contains nothing
 			else
 			{
 				contents = "nothing";
 			}
+			// Return the bank statement message to the player
 			player.sendPrivateText(bankNameMapping + " contains " + contents);
 			return true;
 		}
+		// If the player is not the owner
 		else if (player.getName() != null)
 		{
+			// Remove the scroll
 			splitOff(1);
+			
+			// Inform the player that they cannot use bank scrolls not belonging to them
 			player.sendPrivateText("You cannot view another player's bank statement");	
 			player.notifyWorldAboutChanges();
 			return true;
@@ -75,6 +122,12 @@ public class BankScroll extends PersistentScroll{
 		
 	}
 	
+	/**
+	 * Method to be used for setting the info string of a bank scroll
+	 * by concatenating the player name and the associated bank
+	 * @param player
+	 * @param bankName
+	 */
 	public void setInfo(Player player, String bankName)
 	{
 		setInfoString(player.getName() + " " + bankName);
--- tests/games/stendhal/server/entity/item/scroll/BankScrollTest.java
+++ tests/games/stendhal/server/entity/item/scroll/BankScrollTest.java
@@ -17,48 +17,95 @@ public class BankScrollTest {
 	
 	private static Player player;
 	
+	// Set up the test class by creating a player to be used in all tests
 	@BeforeClass
 	public static void setUpBeforeClass()
 	{
 		player = PlayerTestHelper.createPlayer("bob");
 	}
 	
+	
+	/**
+	 * Test for the result of using a bank scroll when the bank chest is empty and
+	 * when there are two items in the bank chest
+	 */
 	@Test
 	public void shouldReturnChestContents()
 	{
+		// Create two items that will go in the bank 
 		PassiveEntity money = new Money(200);
 		PassiveEntity sword = new Item("sword", "", "", null);
+		
+		// Set the name of the bank to be used (Semos bank)
 		String bank = "bank";
+		
+		// Create scroll and set it's internal information about the player and the
+		// associated bank
+		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
+		scroll.setInfo(player, bank);
+		player.getSlot("bag").add(scroll);
+		
+		// Use the scroll and check if it states that the bank chest contains nothing
+		scroll.useScroll(player);
+		assertEquals("Semos bank chest contains nothing", PlayerTestHelper.getPrivateReply(player));
+		
+		// Add the two items to the bank chest
 		player.getSlot(bank).add(money);
 		player.getSlot(bank).add(sword);
-		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
-		scroll.setInfo(player,bank);
-		player.getSlot("bag").add(scroll);
+
+		// Use the scroll and check if it states that the bank chest contains the two items
 		scroll.useScroll(player);
 		assertEquals("Semos bank chest contains 200 money, 1 sword", PlayerTestHelper.getPrivateReply(player));
 	}
 	
+	/**
+	 * Test to check if a bank scroll remains in bag after use
+	 */
 	@Test
 	public void shouldRemainInBag()
 	{
+		// Set the name of the bank to be used (Semos bank)
 		String bank = "bank";
+		
+		// Create the scroll, set its internal information and add it to the player's bag
+		// as the first item
 		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
 		scroll.setInfo(player,bank);
 		player.equip("bag", scroll);
+		
+		// Use the scroll and check if the first item in the bag is still
+		// the scroll we created before
 		scroll.useScroll(player);
 		assertTrue(scroll.equals(player.getSlot("bag").getFirst()));
 	}
 	
+	/**
+	 * Test to check if a bank scroll disappears when used by a different
+	 * player than its owner
+	 */
 	@Test
 	public void shouldDisappearWhenUsedByWrongPlayer()
 	{
+		// Create a new player different from the one set up for this class
 		Player player1 = PlayerTestHelper.createPlayer("tom");
+		
+		// Set the name of the bank to be used (Semos bank)
 		String bank = "bank";
+		
+		// Create the scroll, set its internal configuration (using the first player), and
+		// add it to the second player's bag
 		BankScroll scroll = (BankScroll) SingletonRepository.getEntityManager().getItem("bank scroll");
 		scroll.setInfo(player, bank);
 		player1.equip("bag", scroll);
+		
+		// Check if the scroll is the first thing in the second player's bag
 		assertTrue(scroll.equals(player1.getSlot("bag").getFirst()));
+		
+		// Have the scroll be used by the second player
 		scroll.useScroll(player1);
+		
+		// Check that the scroll is no longer in the second player's bag and that
+		// they received the appropriate message
 		assertFalse(scroll.equals(player1.getSlot("bag").getFirst()));
 		assertEquals("You cannot view another player's bank statement", PlayerTestHelper.getPrivateReply(player1));
 	}
